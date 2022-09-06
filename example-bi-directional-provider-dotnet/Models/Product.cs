9db39755b805ed369a9f73c44c41f368ef04c07e
AzureAD\MeadsChalcroft <meads.chalcroft@serko.com>
2022-09-07 11:30:38 +1200

    changed back to correct schema type for ID

--- example-bi-directional-provider-dotnet/Models/Product.cs
+++ example-bi-directional-provider-dotnet/Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public string id { get; set; }
+        public int id { get; set; }
         public string type { get; set; }
         public string name { get; set; }
     }
